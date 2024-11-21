b0870c3e5441e47674b4eb1cfbae0127efa063d4
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-11-21 09:36:28 +0100

    fix(contributor): copy address icons #353

--- src/components/DisplayAddress.tsx
+++ src/components/DisplayAddress.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { getDegenOrEnsName } from '@/utils/web3';
 import Link from 'next/link';
 import { Chain } from '@/utils/types';
+import { toast } from 'react-toastify';
+import { CopyDoneIcon, CopyIcon } from '@/components/global/Icons';
 
 export default function DisplayAddress({
   chain,
@@ -10,6 +13,8 @@ export default function DisplayAddress({
   chain: Chain;
   address: string;
 }) {
+  const [isCopied, setCopied] = useState(false);
+
   const walletDisplayName = useQuery({
     queryKey: ['getWalletDisplayName', address, chain.slug],
     queryFn: () =>
@@ -20,12 +25,31 @@ export default function DisplayAddress({
   });
 
   return (
-    <Link
-      href={`/${chain.slug}/account/${address}`}
-      className='overflow-hidden lg:w-[25ch] w-[15ch] overflow-ellipsis hover:text-gray-200'
-    >
-      {walletDisplayName.data ?? address}
-    </Link>
+    <div className='flex items-center lg:w-[29ch] w-[15ch]'>
+      <Link
+        href={`/${chain.slug}/account/${address}`}
+        className=' hover:text-gray-200 max-w-[29ch]'
+      >
+        {walletDisplayName.data ?? address}
+      </Link>
+      <span
+        onClick={() => {
+          setCopied(true);
+          navigator.clipboard.writeText(address);
+          toast.success('Address copied to clipboard');
+          setTimeout(() => {
+            setCopied(false);
+          }, 1000);
+        }}
+        className='ml-2 cursor-pointer hover:text-gray-200'
+      >
+        {isCopied ? (
+          <CopyDoneIcon width={20} height={20} />
+        ) : (
+          <CopyIcon width={20} height={20} />
+        )}
+      </span>
+    </div>
   );
 }
 
